39a27bca0114e412999eb269c13181ae9dd97539
mahdi <mahdirakizadeh@gmail.com>
2023-07-18 13:27:45 +0330

    fixed bug from home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from blog.models import BlogModel, Image
+from contact_us.forms import ContactForm
 
 
 def HomeView(request):
-    return render(request, 'home/index.html')
+    blog = BlogModel.objects.all()
+    imgb = Image.objects.all()
+    contact = ContactForm()
+    return render(request, 'home/index.html', {'blog': blog, 'imgb': imgb, 'contact': contact})
