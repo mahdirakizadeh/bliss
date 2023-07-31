bfe59952e71e1fb6d56735e1e485c47d93489a80
mahdi <mahdirakizadeh@gmail.com>
2023-07-31 16:51:33 +0330

    fixed view of main page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.HomeView, name='home')
+    path('', views.HomeView, name='home'),
+    path('category/<slug:category_slug>/', views.HomeView, name='category_filter'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 from blog.models import BlogModel, Image
+from products.models import ProductModel, CategoryModel
 from contact_us.forms import ContactForm
 
 
-def HomeView(request):
+def HomeView(request, category_slug=None):
     blog = BlogModel.objects.all()
     imgb = Image.objects.all()
     contact = ContactForm()
-    return render(request, 'home/index.html', {'blog': blog, 'imgb': imgb, 'contact': contact})
+    products = ProductModel.objects.filter(available=True)
+    categories = CategoryModel.objects.filter(is_sub=False)
+    if category_slug:
+        category = CategoryModel.objects.get(slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'home/index.html', {'blog': blog, 'imgb': imgb, 'contact': contact, 'products': products,
+                                               'categories': categories})
